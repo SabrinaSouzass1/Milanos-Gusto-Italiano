ca7d360840791b5ac03cb234cd05a4f30c03a63c
Sabrina Souza <76924883+SabrinaSouzass1@users.noreply.github.com>
2023-10-10 19:51:33 -0300

    Personalização dos botões

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,3 +1,6 @@
+
+/* Comidas */
+
 const botoes = document.querySelectorAll(".botao");
 const comidas = document.querySelectorAll(".comida");
  
@@ -19,4 +22,29 @@ function desselecionarComida() {
 function desselecionarBotao() {
 	const botaoSelecionada = document.querySelector(".botao.selecionada");
 	botaoSelecionada.classList.remove("selecionada");
+}
+
+
+/* Bebidas */
+
+const botoesbebidas = document.querySelectorAll('.botao')
+const bebidas = document.querySelectorAll('.bebida')
+
+botoesbebidas.forEach((botao, indice) => {
+	botao.addEventListener("click", () => {
+		desselecionarBotaoBebida();
+		desselecionarBebida();
+
+		botao.classList.add("selecionada");
+	} )
+} )
+
+function desselecionarBotaoBebida() {
+	const botaobebidaSelecionada = document.querySelectorAll(".botao.selecionada")
+	botaobebidaSelecionada.classList.remove("selecionada");
+}
+
+function desselecionarBebida() {
+	const bebidaSelecionada = document.querySelectorAll(".bebida.selecionada")
+	bebidaSelecionada.classList.remove("selecionada");
 }
