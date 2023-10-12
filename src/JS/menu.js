d37be270827f97171556811aa465ee0d99d89405
Sabrina Souza <76924883+SabrinaSouzass1@users.noreply.github.com>
2023-10-12 15:22:18 -0300

    JS

---
+++ src/JS/menu.js
@@ -0,0 +1,10 @@
+
+/* Menu Responsivo */
+
+function clickMenu() {
+    if (botoesid.style.display == 'flex') {
+        botoesid.style.display = 'none'
+    }
+
+    else (botoesid.style.display = 'flex')
+}
