dc88851391045607d202224413dfcedada481961
Sabrina Souza <76924883+SabrinaSouzass1@users.noreply.github.com>
2023-10-12 15:14:23 -0300

    Resolução de JS

--- src/JS/menu.js
+++
@@ -1,9 +0,0 @@
-/* Menu Responsivo */
-
-function clickMenu() {
-    if (botoesid.style.display == 'flex') {
-        botoesid.style.display = 'none' 
-    }
-           
-    else (botoesid.style.display = 'flex')
-}
